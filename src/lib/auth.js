2d8a286377b324ed518eb818c4d3cc2d0fb92239
Nazirul <nazirul.me01c@gmail.com>
2025-10-17 16:12:38 +0600

    Add advanced cookie settings to authentication configuration in `auth.js`
    - Introduced advanced cookie attributes for state management, including `sameSite` and `secure` options to enhance security.

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,6 +7,16 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
   }),
+  advanced: {
+    cookies: {
+      state: {
+        attributes: {
+          sameSite: "none",
+          secure: true,
+        },
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
